a94a2079fb75fd823d639edd865b2767f5cb8c65
Haodong Zheng <a454400098@icloud.com>
2019-03-20 15:03:13 -0400

    create two pages, todo: add nav bar

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(debug=True)
