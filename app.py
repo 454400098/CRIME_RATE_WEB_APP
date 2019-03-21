b3701de5b65a5d546e7536075c42369f4860fde1
Haodong Zheng <a454400098@icloud.com>
2019-03-21 17:51:29 -0400

    test post

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,request
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -13,6 +13,12 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/result",methods = ['POST','GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+    return render_template("result.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
