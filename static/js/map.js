ec42f702b96968bef07d582e78646da049da0ddf
Haodong Zheng <a454400098@icloud.com>
2019-03-21 20:46:04 -0400

    too slow man...;

--- static/js/map.js
+++
@@ -1,7 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiNDU0NDAwMDk4IiwiYSI6ImNqdGo3ZWNwcTBxamE0M2xlcWhmbDI1aWwifQ.NFAQsnKRRN3wTsQPjV-ZtA';
-
-
-var map = new mapboxgl.Map({
-  container: 'map', // container id
-  style:'https://api.mapbox.com/styles/v1/454400098/cjtj816k20my01fnxl19f0zyb.html?fresh=true&title=true&access_token=pk.eyJ1IjoiNDU0NDAwMDk4IiwiYSI6ImNqdGo3ZWNwcTBxamE0M2xlcWhmbDI1aWwifQ.NFAQsnKRRN3wTsQPjV-ZtA#12.0/48.866500/2.317600/0'
-});
