dead9bbd80e12bcc512e2a12b530bb118177cb9f
Haodong Zheng <a454400098@icloud.com>
2019-03-23 16:14:35 -0400

    connection between mongo with front end done, todo: draw chart for first graph

--- app.py
+++ app.py
@@ -2,14 +2,42 @@ from flask import Flask
 from flask import render_template,request
 from flask_bootstrap import Bootstrap
 from pymongo import MongoClient
+import json
+from bson import json_util
+from bson.json_util import dumps
 
 app = Flask(__name__)
 Bootstrap(app)
+
+
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'first'
+COLLECTION_NAME = 'projects'
+FIELDS = {'state': True, 'city_or_county': True, 'address': True, 'congressional_district': True, 'latitude': True,'longitude': True ,'_id': False}
+
+
+
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
+
+@app.route("/first/projects")
+def firset_projects():
+    connection = MongoClient(MONGODB_HOST,MONGODB_PORT)
+    collection = connection[DBS_NAME][COLLECTION_NAME]
+    projects = collection.find(projection=FIELDS)
+    json_projects = []
+    for project in projects:
+        json_projects.append(project)
+    json_projects = json.dumps(json_projects, default=json_util.default)
+    connection.close()
+    return json_projects    
 
 @app.route("/about")
 def about():
@@ -20,6 +48,9 @@ def result():
     if request.method == 'POST':
         result = request.form
     return render_template("result.html",result = result)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
