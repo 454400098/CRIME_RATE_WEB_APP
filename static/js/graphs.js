8ddd9505a366aafa6bc2157f11168f27dbfa29f9
Haodong Zheng <a454400098@icloud.com>
2019-03-24 16:56:58 -0400

    update

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,46 +1,20 @@
 
-console.log('test');
-var payments = crossfilter([
-  {date: "2011-11-14T16:17:54Z", quantity: 2, total: 190, tip: 100, type: "tab", productIDs:["001"]},
-  {date: "2011-11-14T16:20:19Z", quantity: 2, total: 190, tip: 100, type: "tab", productIDs:["001", "005"]},
-  {date: "2011-11-14T16:28:54Z", quantity: 1, total: 300, tip: 200, type: "visa", productIDs:["004" ,"005"]},
-  {date: "2011-11-14T16:30:43Z", quantity: 2, total: 90, tip: 0, type: "tab", productIDs:["001", "002"]},
-  {date: "2011-11-14T16:48:46Z", quantity: 2, total: 90, tip: 0, type: "tab", productIDs:["005"]},
-  {date: "2011-11-14T16:53:41Z", quantity: 2, total: 90, tip: 0, type: "tab", productIDs:["001", "004" ,"005"]},
-  {date: "2011-11-14T16:54:06Z", quantity: 1, total: 100, tip: 0, type: "cash", productIDs:["001", "002", "003", "004" ,"005"]},
-  {date: "2011-11-14T16:58:03Z", quantity: 2, total: 90, tip: 0, type: "tab", productIDs:["001"]},
-  {date: "2011-11-14T17:07:21Z", quantity: 2, total: 90, tip: 0, type: "tab", productIDs:["004" ,"005"]},
-  {date: "2011-11-14T17:22:59Z", quantity: 2, total: 90, tip: 0, type: "tab", productIDs:["001", "002", "004" ,"005"]},
-  {date: "2011-11-14T17:25:45Z", quantity: 2, total: 200, tip: 0, type: "cash", productIDs:["002"]},
-  {date: "2011-11-14T17:29:52Z", quantity: 1, total: 200, tip: 100, type: "visa", productIDs:["004"]}
-]);
-
-var paymentsByTotal = payments.dimension(function(d) { return d.total; });
-
-var paymentsByProductID = payments.dimension(function(d) { return d.productIDs; }, true);
-var ans = paymentsByProductID.top(Infinity);
-console.log(ans);
-
-
 queue()     //asynchronous call back, when all data loaded , continue to call make graphs
     .defer(d3.json, "/first1/projects")
     .defer(d3.json, "static/geojson/us-states.json")
     .await(makeGraphs);
 
 function makeGraphs(error, projectsJson, statesJson) {    //pass db.proejcts and us-states to function
-  console.log('???')
-  console.log('???')
   var crimeProjects = projectsJson;
-  var dateFormat = d3.time.format("%Y-%m-%d");
+  // var dateFormat = d3.time.format("%Y-%m-%d");
+  var dateFormat = d3.time.format("%-m/%-d/%Y");
   crimeProjects.forEach(function(d){
     d["date"] = dateFormat.parse(d["date"]);
     d["date"].setDate(1);
-
   });
 
 
   var ndx = crossfilter(crimeProjects);
-
   //Define Dimensions
   var dateDim = ndx.dimension(function(d) { return d["date"]; });
   var stateDim = ndx.dimension(function(d) { return d["state"]; });
